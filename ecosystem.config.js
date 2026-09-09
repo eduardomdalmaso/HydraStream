e2c6666770124814e41e30de65f2282e00e3e68e
eduardomdalmaso <eduardomdalmaso18@hotmail.com>
2026-09-09 20:55:50 -0300

    feat: add hydra-vms-api service configuration to ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -43,7 +43,21 @@ module.exports = {
       },
     },
 
-    // 4. HydraVMS - Painel de Controle e Orquestrador Web (porta 5173)
+    // 4. HydraVMS Backend - Control Plane REST API & WebSockets (porta 8083)
+    {
+      name: "hydra-vms-api",
+      cwd: "/home/hades/Documents/HydraVMS",
+      script: "./bin/hydravms",
+      interpreter: "none",
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 2000,
+      env: {
+        PORT: "8083",
+      },
+    },
+
+    // 5. HydraVMS Frontend - Painel de Controle e Orquestrador Web (porta 5173)
     {
       name: "hydra-vms",
       cwd: "/home/hades/Documents/HydraVMS/web",
